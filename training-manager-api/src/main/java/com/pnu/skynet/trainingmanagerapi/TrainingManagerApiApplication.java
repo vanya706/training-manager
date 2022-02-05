96d0f00cb89b3bbb0a19199ddd8592b44846b633
Ivan Mostovyi <vanya.mostoviy@gmail.com>
2022-02-05 17:11:40 +0200

    init training-manager-api module

---
+++ training-manager-api/src/main/java/com/pnu/skynet/trainingmanagerapi/TrainingManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.pnu.skynet.trainingmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingManagerApiApplication.class, args);
+    }
+
+}
---
+++ training-manager-api/src/test/java/com/pnu/skynet/trainingmanagerapi/TrainingManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnu.skynet.trainingmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

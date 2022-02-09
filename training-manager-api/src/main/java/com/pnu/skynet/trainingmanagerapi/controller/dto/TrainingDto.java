c7fcedcbba379e7cb8abd41ffd458bb1d383cd93
Ivan Mostovyi <vanya.mostoviy@gmail.com>
2022-02-10 00:24:40 +0200

    Fixed backend build

--- training-manager-api/src/main/java/com/pnu/skynet/trainingmanagerapi/controller/dto/TrainingDto.java
+++ training-manager-api/src/main/java/com/pnu/skynet/trainingmanagerapi/controller/dto/TrainingDto.java
@@ -2,13 +2,12 @@ package com.pnu.skynet.trainingmanagerapi.controller.dto;
 
 import com.pnu.skynet.trainingmanagerapi.constant.Color;
 
-import javax.validation.constraints.NotNull;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
 public record TrainingDto(
+        String id,
         Color color,
-        @NotNull
         LocalDateTime startDate,
         Duration duration) {
 }

c2bf2251be86f88e9cad61a90656e7663fae513e
Ivan Mostovyi <vanya.mostoviy@gmail.com>
2022-02-10 00:14:50 +0200

    Fixed freemarker dependency

---
+++ training-manager-api/src/main/java/com/pnu/skynet/trainingmanagerapi/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.pnu.skynet.trainingmanagerapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import static com.pnu.skynet.trainingmanagerapi.constant.FlashMessageConstants.FLASH_MESSAGE_ERROR;
+import static com.pnu.skynet.trainingmanagerapi.constant.FlashMessageConstants.FLASH_MESSAGE_SUCCESS;
+import static java.util.Objects.nonNull;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model, String error, String logout) {
+
+        if (nonNull(error))
+            // todo add localization to message in the future
+            model.addAttribute(FLASH_MESSAGE_ERROR, "Невірне ім'я користувача або пароль");
+
+        if (nonNull(logout))
+            // todo add localization to message in the future
+            model.addAttribute(FLASH_MESSAGE_SUCCESS, "Ви успішно вийшли");
+
+        return "users/login";
+    }
+}
+
